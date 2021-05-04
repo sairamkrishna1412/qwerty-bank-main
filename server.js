d1dfda1d50c9302e8cdb918d296ac3734e6666d1
sai ram krishna <sairamkrishna1412@gmail.com>
2021-05-04 18:25:41 +0530

    DB connection added

---
+++ server.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connection established!"));
