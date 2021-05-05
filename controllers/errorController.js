c516b73425991fe588bd62ec34e9e006574e9676
sai ram krishna <sairamkrishna1412@gmail.com>
2021-05-05 22:31:16 +0530

    Added error handling dev

---
+++ controllers/errorController.js
@@ -0,0 +1,15 @@
+function sendErrorDev(err, req, res) {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack,
+    });
+}
+module.exports = function (err, req, res, next) {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "fail";
+    if (process.env.NODE_ENV == "development") {
+        sendErrorDev(err, req, res);
+    }
+};
