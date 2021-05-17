a30a20e38fdfbf171eded4624b7b53f2985474c6
sai ram krishna <sairamkrishna1412@gmail.com>
2021-05-17 12:14:21 +0530

    bug fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,7 +9,8 @@ function sendErrorDev(err, req, res) {
 module.exports = function (err, req, res, next) {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "fail";
-    if (process.env.NODE_ENV == "development") {
-        sendErrorDev(err, req, res);
-    }
+    sendErrorDev(err, req, res);
+    // if (process.env.NODE_ENV == "development") {
+    //     sendErrorDev(err, req, res);
+    // }
 };
